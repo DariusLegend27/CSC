64215fbe4c76c98a38714bcf0efde88a3a6bbf18
Zion Jiam <zionjiam88.18@gmail.com>
2020-06-04 17:05:56 +0800

    Register User Task  3

---
+++ Task_3_Bearer/Task_3_Bearer/Task_3_Bearer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Task_3_Bearer.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
