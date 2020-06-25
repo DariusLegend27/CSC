f625108dab988f90bdeb055b2cc1e512ae20f420
Gary Ang <wei_bo99@hotmail.com>
2020-06-25 22:56:29 +0800

    Task 2

---
+++ Task2/Task2/Controllers/ProductsV2Controller.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Task2.Models;
+
+namespace Task2.Controllers
+{
+    public class ProductsV2Controller : ApiController
+    {
+        static readonly IProductRepository repository = new ProductRepository();
+
+        //Get All
+        [HttpGet]
+        [Route("api/v2/products")]
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return repository.GetAll();
+
+        }
+
+        //Get by id
+        public Product GetProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return item;
+        }
+
+        //get by catgory
+        public IEnumerable<Product> GetProductsByCategory(string category)
+        {
+            return repository.GetAll().Where(
+                p => string.Equals(p.Category, category, StringComparison.OrdinalIgnoreCase));
+        }
+
+        //addd
+        public HttpResponseMessage PostProduct(Product item)
+        {
+            item = repository.Add(item);
+            var response = Request.CreateResponse<Product>(HttpStatusCode.Created, item);
+
+            string uri = Url.Link("DefaultApi", new { id = item.Id });
+            response.Headers.Location = new Uri(uri);
+            return response;
+        }
+
+        //Update
+        public void PutProduct(int id, Product product)
+        {
+            product.Id = id;
+            if (!repository.Update(product))
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+        }
+
+        //remove
+        public void DeleteProduct(int id)
+        {
+            Product item = repository.Get(id);
+            if (item == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            repository.Remove(id);
+        }
+
+
+
+
+    }
+}
